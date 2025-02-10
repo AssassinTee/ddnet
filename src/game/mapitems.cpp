d4273cec829dd2f526e9a0361299d9626e8272b0
Marvin Winkens <m.winkens@fz-juelich.de>
2025-02-10 14:46:57 +0100

    add better speedtile variant

--- src/game/mapitems.cpp
+++ src/game/mapitems.cpp
@@ -78,7 +78,7 @@ bool IsTeleTileNumberUsedAny(int Index)
 
 bool IsValidSpeedupTile(int Index)
 {
-	return Index == TILE_BOOST;
+	return Index == TILE_BOOST || Index == TILE_BOOST2;
 }
 
 bool IsValidSwitchTile(int Index)
